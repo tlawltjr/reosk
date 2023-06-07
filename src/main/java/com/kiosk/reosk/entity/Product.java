fa453682fdf716ebf17fd2260174e99c899d5f53
jjg19 <jjg19@TFG155>
2023-06-07 13:20:55 +0900

    z

--- src/main/java/com/kiosk/reosk/entity/Product.java
+++ src/main/java/com/kiosk/reosk/entity/Product.java
@@ -1,9 +1,19 @@
 package com.kiosk.reosk.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
 
     private int pCode;
